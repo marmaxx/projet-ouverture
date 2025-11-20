252af37260308f1cce65ded549f7e7ba68dfa6e3
Sekou Cissé <sekoucisse.pr@gmail.com>
2025-11-20 14:38:08 +0100

    Ajout de commentaire pour tous les cas de Remy + beauty printing dans le cas de Tree.ml

--- projet_ouverture/bin/main.ml
+++ projet_ouverture/bin/main.ml
@@ -1,3 +1,15 @@
+(* Pour Remy
+open Projet_ouverture.Tree
+open Projet_ouverture.Remy
+*)
+
+(* Pour RemyRef
+open Projet_ouverture.TreeRef
+open Projet_ouverture.RemyRef
+open Projet_ouverture.TreeDot
+*)
+
+(* Pour RemyRefABR *)
 open Projet_ouverture.TreeRef
 open Projet_ouverture.RemyRefABR
 open Projet_ouverture.TreeDot
@@ -5,6 +17,24 @@ open Projet_ouverture.TreeDot
 let () =
   Random.self_init ();
 
+(* Pour Remy
+  print_string "Entrez un nombre n : ";
+  let n = read_int () in
+  let arbre = Empty 0 in
+  let arbre_final = algoRemy arbre n 1 in
+  print_tree arbre_final
+  *)
+
+(* Pour RemyRef :
+  print_string "Entrez un nombre n : ";
+  let n = read_int () in
+  let list_node = Array.init (2 * n+1) (fun _ -> ref Empty) in
+  algoRemyRef n 1 list_empty;
+  export_tree_to_dot "arbre.dot" !(list_empty.(0));
+  print_endline "Fichier arbre.dot généré !"
+  *)
+
+(* Pour RemyRefABR : *)
   print_string "Entrez un nombre n : ";
   let n = read_int () in
   let list_empty = Array.init (n+1) (fun _ -> ref Empty) in
@@ -12,12 +42,3 @@ let () =
   algoRemyRefABR n 1 list_empty;
   export_tree_to_dot "arbre.dot" !(arbre);
   print_endline "Fichier arbre.dot généré !"
-
-  (* Pour RemyRef :
-      print_string "Entrez un nombre n : ";
-      let n = read_int () in
-      let list_node = Array.init (2 * n+1) (fun _ -> ref Empty) in
-      algoRemyRef n 1 list_empty;
-      export_tree_to_dot "arbre.dot" !(list_empty.(0));
-      print_endline "Fichier arbre.dot généré !"
-  *)
--- projet_ouverture/lib/Tree.ml
+++ projet_ouverture/lib/Tree.ml
@@ -8,3 +8,14 @@ let getValue t =
   match t with
   | Empty a -> a
   | Node (v, _, _) -> v
+
+(* Affichage simple de l'arbre dans le terminal *)
+let rec print_tree ?(prefix="") ?(is_left=true) tree =
+match tree with
+| Empty v ->
+    Printf.printf "%s%sEmpty(%d)\n" prefix (if is_left then "├─ " else "└─ ") v
+| Node (v, l, r) ->
+    Printf.printf "%s%sNode(%d)\n" prefix (if is_left then "├─ " else "└─ ") v;
+    let new_prefix = prefix ^ (if is_left then "│  " else "   ") in
+    print_tree ~prefix:new_prefix ~is_left:true l;
+    print_tree ~prefix:new_prefix ~is_left:false r
